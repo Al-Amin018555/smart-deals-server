f80f5d25099fa6e6d343bdaef59ad0151dba953b
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-04-08 14:00:29 +0600

    mongodb connected,created a post api

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
+require('dotenv').config()
 const express = require('express');
+var cors = require('cors')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(express.json())
+
+//smartdb
+//MjdN5YkIKcj5lW8C
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q3bebek.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const smartdb = client.db("smartdb");
+        const productsCollection = smartdb.collection("products");
+
+        app.post("/products", async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("smart server is running")
